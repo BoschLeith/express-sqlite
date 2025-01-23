b18359f615974e396580a5de40a5edd6135236fb
BoschLeith <bosch.leith@gmail.com>
2025-01-23 10:18:32 -0600

    Serve static files and update root route to send index.html

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,19 @@
-import express from "express";
 import type { Request, Response } from "express";
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
 
-const app = express();
 const PORT = process.env.PORT || 3000;
+const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "../public")));
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello, TypeScript with Express!");
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.listen(PORT, () => {
